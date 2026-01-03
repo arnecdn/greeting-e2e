87aff7130e5fa612f3b5dfb506d941374fe49b5c
Arne Nilsen <arne.nilsen@skatteetaten.no>
2026-01-04 00:54:36 +0100

    Refactored and added more debug output

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use crate::greeting_receiver::{generate_random_message, GreetingCmd, GreetingRec
 use clap::Parser;
 use confy::ConfyError;
 use std::collections::HashMap;
-use std::thread;
-use std::thread::Thread;
+use std::str::FromStr;
+use log::error;
 use thiserror::Error;
-use tracing::info;
+use tracing::{debug, info};
 use tracing::Level;
 
 mod api;
@@ -17,9 +17,10 @@ mod greeting_receiver;
 #[tokio::main]
 async fn main() -> Result<(), E2EError> {
     // FmtSubscriber logs to stdout by default
-    tracing_subscriber::fmt().with_max_level(Level::INFO).init();
-
     let args = CliArgs::parse();
+
+    tracing_subscriber::fmt().with_max_level(Level::from_str(&args.logging).unwrap()).init();
+
     let cfg = load_e2e_config(&args.config_path)?;
     info!("Loaded E2E config: {:?}", cfg);
 
@@ -33,23 +34,24 @@ async fn main() -> Result<(), E2EError> {
     info!("Log-entry offset-id: {}", offset);
     let greeting_receiver_client = GreetingReceiverClient::new_client(cfg.greeting_receiver_url);
 
-    let test_messages = (0..cfg.num_iterations)
+    let mut tasks = (0..cfg.num_iterations)
         .map(|_| generate_random_message())
-        .fold(vec![], |mut acc, m| {
-            acc.push(m);
-            acc
-        });
-
-    let mut tasks = HashMap::new();
-    for m in test_messages {
-        let resp = greeting_receiver_client.send(m.clone()).await?;
-        let task = TestTask {
+        .map(|m| TestTask {
             external_reference: m.external_reference.to_string(),
             message: m,
             greeting_logg_entry: None,
-        };
-        tasks.insert(task.external_reference.to_string(), task);
+        })
+        .fold(HashMap::new(), |mut acc, t| {
+            acc.insert(t.external_reference.to_string(), t);
+            acc
+        });
+    info!("Generated {} test tasks", &tasks.len());
+
+    for t in &tasks {
+        greeting_receiver_client.send(t.1.message.clone()).await?;
+        debug!("Sent message: {:?}", t.1.message.external_reference);
     }
+
     let mut current_offset = offset;
 
     while current_offset == greeting_api_client.get_last_log_entry().await?.unwrap().id {
@@ -57,13 +59,12 @@ async fn main() -> Result<(), E2EError> {
     }
 
     loop {
-        info!("Checking log entries from offset-id: {:?}", current_offset);
-        let mut log_entries = greeting_api_client
+        let log_entries = greeting_api_client
             .get_log_entries(current_offset + 1, cfg.greeting_log_limit)
             .await?;
 
-        info!("Found {:?} entries from offset-id: {}", &log_entries.len(), offset);
-        current_offset = log_entries.iter().map(|l| l.id).max().or_else(|| Some(offset)).unwrap();
+        debug!("Found {:?} entries from offset-id: {}", &log_entries.len(), offset);
+        let temp_offset = log_entries.iter().map(|l| l.id).max().or_else(|| Some(offset)).unwrap();
 
         for log_entry in log_entries {
             if let Some(entry) = tasks.get_mut(&log_entry.external_reference) {
@@ -72,27 +73,29 @@ async fn main() -> Result<(), E2EError> {
         }
 
         if tasks.iter().all(|e| e.1.greeting_logg_entry.is_some()) {
-            printTestResult(&mut tasks);
+            print_test_result(&mut tasks);
             break;
         }
+
+        current_offset = temp_offset;
     }
 
     Ok(())
 }
 
 
-fn printTestResult(tasks: &mut HashMap<String, TestTask>) {
+fn print_test_result(tasks: &mut HashMap<String, TestTask>) {
+    info!("Verified {} test-tasks",&tasks.len());
     for ctx in tasks {
         let msg = &ctx.1.message;
         let gle = &ctx.1.greeting_logg_entry.as_ref().unwrap();
 
-        info!("Verified logg-id: {:?}, greeting.created: {:?}, log.created: {:?}",
+        debug!("Verified logg-id: {:?}, greeting.created: {:?}, log.created: {:?}",
                     gle.id,
                     msg.created,
                     gle.created
                 );
     }
-    info!("All messages verified");
 }
 
 #[derive(Debug)]
@@ -112,6 +115,15 @@ pub(crate) struct CliArgs {
         env = "greeting-e2e-test-config"
     )]
     pub config_path: String,
+
+    /// Enable debug mode
+    #[arg(
+        short = 'd',
+        long = "debug",
+        env = "greeting-e2e-test-debug",
+        default_value = "info"
+    )]
+    pub logging: String,
 }
 
 #[derive(Error, Debug)]
